5278f038767cc8b09bfef4611a4d1eb9271bd150
naxinyi <780957235@qq.com>
2019-10-12 20:04:02 +0800

    10.12

--- PHP/复习/test1.php
+++ PHP/复习/test1.php
@@ -19,3 +19,25 @@ echo '<br/>';
 
 $t=date('Y-m-d');
 echo $t;
+
+
+// 响应报文的应用场景
+/**
+ * 设置文件响应类型：header('Content-type:text/css');  它的值还可以为application/javascript;text/html;text/plain
+ * 设置重定向：header('Location:https://www.baidu.com')
+ * 下载文件：让文件下载和默认文件下载（Content-Type和Content-Disposition）
+ */
+
+/**
+ * GET请求的参数不会保存到浏览器的历史里，但是POST的请求参数会保存到浏览器的历史里
+ * 与POST相比，GET请求的安全性差
+ * GET：表单数据通过URL中的?参数传递到服务端
+ * POST：表单数据是通过请求提传递到服务端的
+ */
+
+/**
+ * form标签的两个属性action：放的是表单提交的地址；method：放的是表单以什么方式进行了提交
+ * $_REQUEST：获取get或者是post方法提交的内容
+ * 获取当前页面的访问路径：$_SERVER[PHP_SELF]
+ * 判断表单是否提交成功：$_SERVER['REQUEST_METHOD']
+ */
