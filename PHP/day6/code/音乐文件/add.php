7a10cbc628994ddabdf626dcf4ad82185e44f87f
naxinyi <780957235@qq.com>
2019-10-13 20:36:32 +0800

    10.13

--- PHP/day6/code/音乐文件/add.php
+++ PHP/day6/code/音乐文件/add.php
@@ -3,7 +3,7 @@
 function add () {
   // 目标：接收客户端提交的数据和文件，最终保存到数据文件中
   $data = array(); // 准备一个空的容器，用来装最终要保存的 数据
-  $data['id'] = uniqid();
+  $data['id'] = uniqid(); // 生成当前唯一的id
 
   // 1. 接收提交的文本内容
   // ===================================================
@@ -121,43 +121,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <title>添加新音乐</title>
-  <link rel="stylesheet" href="bootstrap.css">
+    <meta charset="UTF-8">
+    <title>添加新音乐</title>
+    <link rel="stylesheet" href="bootstrap.css">
 </head>
+
 <body>
-  <div class="container py-5">
-    <h1 class="display-4">添加新音乐</h1>
-    <hr>
+    <div class="container py-5">
+        <h1 class="display-4">添加新音乐</h1>
+        <hr>
 
-    <?php if (isset($error_message)): ?>
-    <div class="alert alert-danger">
-      <?php echo $error_message; ?>
+        <?php if (isset($error_message)): ?>
+        <div class="alert alert-danger">
+            <?php echo $error_message; ?>
+        </div>
+        <?php endif ?>
+
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="title">标题</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+            <div class="form-group">
+                <label for="artist">歌手</label>
+                <input type="text" class="form-control" id="artist" name="artist">
+            </div>
+            <div class="form-group">
+                <label for="images">海报</label>
+                <!-- multiple 可以让一个文件域多选 -->
+                <input type="file" class="form-control" id="images" name="images[]" accept="image/*" multiple>
+            </div>
+            <div class="form-group">
+                <label for="source">音乐</label>
+                <!-- accept 可以设置两种值分别为  MIME Type / 文件扩展名 -->
+                <input type="file" class="form-control" id="source" name="source" accept="audio/*">
+            </div>
+            <button class="btn btn-primary btn-block">保存</button>
+        </form>
     </div>
-    <?php endif ?>
-
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-      <div class="form-group">
-        <label for="title">标题</label>
-        <input type="text" class="form-control" id="title" name="title">
-      </div>
-      <div class="form-group">
-        <label for="artist">歌手</label>
-        <input type="text" class="form-control" id="artist" name="artist">
-      </div>
-      <div class="form-group">
-        <label for="images">海报</label>
-        <!-- multiple 可以让一个文件域多选 -->
-        <input type="file" class="form-control" id="images" name="images[]" accept="image/*" multiple>
-      </div>
-      <div class="form-group">
-        <label for="source">音乐</label>
-        <!-- accept 可以设置两种值分别为  MIME Type / 文件扩展名 -->
-        <input type="file" class="form-control" id="source" name="source" accept="audio/*">
-      </div>
-      <button class="btn btn-primary btn-block">保存</button>
-    </form>
-  </div>
 </body>
+
 </html>
---
+++ PHP/复习/音乐文件/list.php
@@ -0,0 +1,9 @@
+<?php
+
+$data=[];
+$data['image']=array();
+var_dump($data);
+echo array_values($data['image']);
+echo array_keys($data['image']);
+
+?>
