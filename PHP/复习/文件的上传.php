35ba85ee0c0f0a719e766a0e8c5c63bd803caa85
naxinyi <780957235@qq.com>
2019-10-14 23:46:04 +0800

    10.14

--- PHP/复习/文件的上传.php
+++ PHP/复习/文件的上传.php
@@ -1,5 +1,32 @@
 <?php
 
+if(isset($_POST['sub'])){
+    $sfile=$_FILES['sfile'];
+    $name=$sfile['name'];
+    $arr=explode('.',$name);
+    $len=count($arr)-1;
+    $newarr=array('txt','md','exe');
+    $flag=true;
+    for($i=0;$i<count($newarr);$i++){
+        if($newarr[i]==arr[len]){
+            $flag=false;
+        }
+    }
+    if($flag==false){
+        echo "<script> alert(’上传文件失败‘)</script?"
+    }else{
+        $imgname=time();
+        $img=$imgname.'.'.$arr[$len];
+        $baseurl=getcwd();
+        $bb=move_uploaded_file($sfile['tmp_name'],$baseue,'/upload'.$img);
+        if($bb){
+            echo "success";
+        }else{
+            echo "fail"
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
