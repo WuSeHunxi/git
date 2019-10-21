2353397dac0c7ad9e76f2b3c0a6e056bed951810
naxinyi <780957235@qq.com>
2019-10-21 20:18:42 +0800

    10.21

---
+++ PHP/NEW/10.21/小表单的提交.php
@@ -0,0 +1,19 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post"></form>
+</body>
+
+</html>
