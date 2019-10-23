76710c6f27892a953d1730c5b9553eae2e061f8e
naxinyi <780957235@qq.com>
2019-10-23 11:42:12 +0800

    10.23

---
+++ PHP/NEW/10.23/数组/测试二维数组.php
@@ -0,0 +1,20 @@
+<?php
+
+$arr=array(
+    array("jaj",10),
+    array("ooo",19)
+);
+
+// foreach ($arr as $key) {
+//     foreach ($key as $col ) {
+//         echo $col.'    ';
+//     }
+// }
+
+for($i=0;$i<count($arr);$i++){
+    // echo $i;
+    // echo 'array['.$i.']'.'=>'.$arr[$i];
+   var_dump($arr[0]);
+}
+
+?>
