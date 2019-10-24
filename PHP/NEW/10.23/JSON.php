b0b3de077866edc6b10223ccf5bd57b8831c8576
naxinyi <780957235@qq.com>
2019-10-24 13:53:10 +0800

    10.24

--- PHP/NEW/10.23/JSON.php
+++ PHP/NEW/10.23/JSON.php
@@ -10,3 +10,18 @@
 
  $arr=array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
  echo json_encode($arr);
+
+ echo "<br/>";
+
+ //将PHP对象转化成了JSON格式的数据
+ class Emp {
+    public $name = "";
+    public $hobbies  = "";
+    public $birthdate = "";
+}
+$e = new Emp();
+$e->name = "sachin";
+$e->hobbies  = "sports";
+// $e->birthdate = date('m/d/Y h:i:s a', "8/5/1974 12:20:03 p");
+$e->birthdate = date('Y-m-d h:i:s a', strtotime("2019-10-23 12:20:03"));
+echo json_encode($e);
