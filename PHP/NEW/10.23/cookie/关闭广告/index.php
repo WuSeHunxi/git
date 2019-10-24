91ff06dedd7ea29a109777535f7b214c930b9809
naxinyi <780957235@qq.com>
2019-10-24 14:44:04 +0800

    10.24

--- PHP/NEW/10.23/cookie/关闭广告/index.php
+++ PHP/NEW/10.23/cookie/关闭广告/index.php
@@ -9,26 +9,33 @@ if (isset($_GET['action']) && $_GET['action'] === 'close-ad') {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <title>Document</title>
-  <style>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
     .ad {
-      height: 200px;
-      background-color: #ff0;
+        height: 200px;
+        background-color: #ff0;
     }
 
     .ad a {
-      float: right;
+        float: right;
     }
-  </style>
+    </style>
 </head>
+
 <body>
-  <?php if (empty($_COOKIE['hide_ad']) || $_COOKIE['hide_ad'] !== '1'): ?>
-  <div class="ad">
-    <!-- <a href="close.php">不再显示</a> -->
-    <a href="index.php?action=close-ad">不再显示</a>
-  </div>
-  <?php endif ?>
+    <!-- 如果没有cookie或者是得到的值!=1那么就显示广告 -->
+    <?php if (empty($_COOKIE['hide_ad']) || $_COOKIE['hide_ad'] !== '1'): ?>
+    <div class="ad">
+        <!-- <a href="close.php">不再显示</a> -->
+
+        <!--在一个页面完成-->
+        <a href="index.php?action=close-ad">不再显示</a>
+
+    </div>
+    <?php endif ?>
 </body>
+
 </html>
