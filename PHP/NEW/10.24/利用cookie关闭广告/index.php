cf60035ccab3e6014e0ceb5bbc1b8feacaacdd75
naxinyi <780957235@qq.com>
2019-10-24 14:47:25 +0800

    10.24

---
+++ PHP/NEW/10.24/利用cookie关闭广告/index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+    a {
+        float: right;
+        cursor: pointer;
+    }
+
+    .box {
+        background: yellow;
+        height: 300px;
+        width: 100%;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="box">
+        <a href="">不再显示广告</a>
+    </div>
+</body>
+
+</html>
