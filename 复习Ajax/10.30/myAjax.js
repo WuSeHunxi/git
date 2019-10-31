d74768ca258e396d8051899c698eb70c065906aa
naxinyi <780957235@qq.com>
2019-10-31 08:20:22 +0800

    10.31

--- 复习Ajax/10.30/myAjax.js
+++ 复习Ajax/10.30/myAjax.js
@@ -9,11 +9,11 @@ function obj2str(obj) {
     obj = obj || {}; // 如果没有传参, 为了添加随机因子,必须自己创建一个对象
     obj.t = new Date().getTime();
     var res = [];
-    for(var key in obj){
+    for (var key in obj) {
         // 在URL中是不可以出现中文的, 如果出现了中文需要转码
         // 可以调用encodeURIComponent方法
         // URL中只可以出现字母/数字/下划线/ASCII码
-        res.push(encodeURIComponent(key)+"="+encodeURIComponent(obj[key])); // [userName=lnj, userPwd=123456];
+        res.push(encodeURIComponent(key) + "=" + encodeURIComponent(obj[key])); // [userName=lnj, userPwd=123456];
     }
     return res.join("&"); // userName=lnj&userPwd=123456
 }
@@ -22,13 +22,11 @@ function ajax(url, obj, timeout, success, error) {
     var str = obj2str(obj); // key=value&key=value;
     // 1.创建一个异步对象
     var xmlhttp, timer;
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
     }
-    else
-    {// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     // 2.设置请求方式和请求地址
     /*
@@ -36,7 +34,7 @@ function ajax(url, obj, timeout, success, error) {
     url：文件在服务器上的位置
     async：true（异步）或 false（同步）
     */
-    xmlhttp.open("GET", url+"?"+str, true);
+    xmlhttp.open("GET", url + "?" + str, true);
     // 3.发送请求
     xmlhttp.send();
     // 4.监听状态的变化
@@ -48,22 +46,22 @@ function ajax(url, obj, timeout, success, error) {
         3: 请求处理中
         4: 请求已完成，且响应已就绪
         */
-        if(xmlhttp.readyState === 4){
+        if (xmlhttp.readyState === 4) {
             clearInterval(timer);
             // 判断是否请求成功
-            if(xmlhttp.status >= 200 && xmlhttp.status < 300 ||
-                xmlhttp.status === 304){
+            if (xmlhttp.status >= 200 && xmlhttp.status < 300 ||
+                xmlhttp.status === 304) {
                 // 5.处理返回的结果
                 // console.log("接收到服务器返回的数据");
                 success(xmlhttp);
-            }else{
+            } else {
                 // console.log("没有接收到服务器返回的数据");
                 error(xmlhttp);
             }
         }
     }
     // 判断外界是否传入了超时时间
-    if(timeout){
+    if (timeout) {
         timer = setInterval(function () {
             console.log("中断请求");
             xmlhttp.abort();
---
+++ 复习Ajax/10.31/复习/ajax-get.php
@@ -0,0 +1,5 @@
+<?php
+
+// if($_SERVER['REQUEST_METHOD']==='get'){
+    echo "jajja";
+// }
